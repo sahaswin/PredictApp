8d0123dbc64460b2a5917997efc1623332dcfd7d
SharkSmile <44153946+sahaswin@users.noreply.github.com>
2024-04-12 16:14:15 -0400

    Updated EDA

--- explore_page.py
+++ explore_page.py
@@ -2,85 +2,146 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
+import plotly.graph_objs as go
 
 # Set the title of the web app
 st.title('Mobile Network Churn EDA')
 
 
-# Load the dataset
 def load_data():
-    data = pd.read_csv('/mount/src/predictapp/feature_values.csv')
-    return data
+    """ Load the dataset from a file """
+    return pd.read_csv('/mount/src/predictapp/feature_values.csv')
 
 
-data = load_data()
+Hcontainer = st.container(border=True)
+Fcontainer = st.container(border=True)
+HMcontainer = st.container(border=True)
+Icontainer = st.container(border=True)
+
+
+def simplify_category_name(data_frame, original_column):
+    """ Simplify category name for better visualization """
+    data_frame[original_column] = data_frame[original_column].apply(lambda x: x.split('_')[-1])
+    return data_frame
+
+
+def generate_summary(data, feature_columns):
+    """ Generate summary data for a feature group """
+    feature_data = data[feature_columns].sum().reset_index()
+    feature_data.columns = ['Category', 'Count']
+    return simplify_category_name(feature_data, 'Category')
+
+
+def generate_heatmap(data, group_x, group_y):
+    """ Generate a heatmap between two feature groups """
+    columns_x = feature_groups[group_x]
+    columns_y = feature_groups[group_y]
+    agg_data = pd.DataFrame(index=columns_x, columns=columns_y, data=0)
+
+    for col_x in columns_x:
+        for col_y in columns_y:
+            count = data[(data[col_x] == True) & (data[col_y] == True)].shape[0]
+            agg_data.loc[col_x, col_y] = count
+
+    agg_data.columns = [col.split('_')[-1] for col in agg_data.columns]
+    agg_data.index = [idx.split('_')[-1] for idx in agg_data.index]
+
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(agg_data, annot=True, fmt="d", cmap='coolwarm')
+    plt.title(f'Heatmap between {group_x} and {group_y}')
+    plt.xlabel(group_y)
+    plt.ylabel(group_x)
+    plt.xticks(rotation=45)
+    plt.yticks(rotation=0)
+    HMcontainer.pyplot(plt)
+
+
+def interactive_feature_graph(data):
+    Icontainer.title('Interactive Feature Graph')
+
+    # Define specific features to plot
+    features_to_plot = ['tenure', 'MonthlyCharges', 'TotalCharges']
+
+    # Create a Plotly graph
+    fig = go.Figure()
+    for feature in features_to_plot:
+        fig.add_trace(go.Scatter(
+            x=data.index,
+            y=data[feature],
+            mode='lines+markers',
+            name=feature
+        ))
+
+    # Update layout
+    fig.update_layout(
+        height=600,
+        title_text="Interactive Plot of Selected Features",
+        xaxis_title="Index",
+        yaxis_title="Value",
+        hovermode="x"
+    )
+    Icontainer.plotly_chart(fig, use_container_width=True)
 
 
 def show_explore_page():
-    st.write(data.head())
-    st.write('Descriptive Statistics:', data.describe())
+    data = load_data()
+    Hcontainer.header('Data Analysis of user Inputs')
+    Hcontainer.markdown('Here you are able to go through the user inputs and an analysis of said data.\n')
 
-    # Visualization of One-Hot Encoded Data
-    st.header('Visualization of Categorical Features')
+    Hcontainer.markdown('Sample of user input data\n')
+    Hcontainer.write(data.head())
+    Hcontainer.markdown('Summary of numerical data in user inputs\n')
+    Hcontainer.write(data.describe())
 
-    # Define the mapping of features to their respective columns
-    feature_groups = {
-        'PaperlessBilling': ['PaperlessBilling_No', 'PaperlessBilling_Yes'],
-        'Partner': ['Partner_No', 'Partner_Yes'],
-        'InternetService': ['InternetService_DSL', 'InternetService_Fiber optic', 'InternetService_No'],
-        'OnlineBackup': ['OnlineBackup_No', 'OnlineBackup_No internet service', 'OnlineBackup_Yes'],
-        'Dependents': ['Dependents_No', 'Dependents_Yes'],
-        'SeniorCitizen': ['SeniorCitizen_No', 'SeniorCitizen_Yes'],
-        'DeviceProtection': ['DeviceProtection_No', 'DeviceProtection_No internet service', 'DeviceProtection_Yes'],
-        'OnlineSecurity': ['OnlineSecurity_No', 'OnlineSecurity_No internet service', 'OnlineSecurity_Yes'],
-        'TechSupport': ['TechSupport_No', 'TechSupport_No internet service', 'TechSupport_Yes'],
-        'Contract': ['Contract_Month-to-month', 'Contract_One year', 'Contract_Two year'],
-        'StreamingTV': ['StreamingTV_No', 'StreamingTV_No internet service', 'StreamingTV_Yes'],
-        'PaymentMethod': ['PaymentMethod_Bank transfer (automatic)', 'PaymentMethod_Credit card (automatic)',
-                          'PaymentMethod_Electronic check', 'PaymentMethod_Mailed check']
-    }
+    Fcontainer.header('Feature Group Summaries')
+    selected_feature_group = Fcontainer.selectbox('Select Feature Group', list(feature_groups.keys()))
 
-    def simplify_category_name(data_frame, original_column):
-        data_frame[original_column] = data_frame[original_column].apply(lambda x: x.split('_')[-1])
-        return data_frame
+    if Fcontainer.checkbox('Show Grouped Data'):
+        summary_data = generate_summary(data, feature_groups[selected_feature_group])
+        Fcontainer.write(summary_data)
 
-    # Generate summary tables for each feature group
-    def generate_summary(data, feature_columns):
-        feature_data = data[feature_columns].sum().reset_index()
-        feature_data.columns = ['Category', 'Count']
-        return simplify_category_name(feature_data, 'Category')
+    Fcontainer.header('Visualization of Categorical Features')
+    chart_type = Fcontainer.radio("Select Chart Type", ('Pie Chart', 'Bar Chart'))
+    if chart_type:
+        feature_data = generate_summary(data, feature_groups[selected_feature_group])
+        if chart_type == 'Pie Chart':
+            fig = px.pie(feature_data, values='Count', names=f'Category',
+                         title=f'Distribution of {selected_feature_group}')
+        elif chart_type == 'Bar Chart':
+            fig = px.bar(feature_data, x='Category', y='Count', title=f'Distribution of {selected_feature_group}',
+                         color='Category')
+        Fcontainer.plotly_chart(fig, use_container_width=True)
 
-    st.header('Feature Group Summaries')
-    selected_feature_group = st.selectbox('Select Feature Group', list(feature_groups.keys()))
+    HMcontainer.header('Heatmaps for specific features')
 
-    # Show aggregated data for selected feature group
-    if st.checkbox('Show Grouped Data'):
-        feature_columns = feature_groups[selected_feature_group]
-        summary_data = generate_summary(data, feature_columns)
-        st.write(f'Summary for {selected_feature_group}:', summary_data)
+    selected_group_x = HMcontainer.selectbox('Select Group X', list(feature_groups.keys()), index=0)
+    selected_group_y = HMcontainer.selectbox('Select Group Y', list(feature_groups.keys()), index=1)
 
-    # Visualization of One-Hot Encoded Data
-    st.header('Visualization of Categorical Features')
-    feature_columns = feature_groups[selected_feature_group]
+    if HMcontainer.button('Generate Heatmap'):
+        generate_heatmap(data, selected_group_x, selected_group_y)
 
-    # Data processing to sum each category
-    feature_data = generate_summary(data, feature_columns)
+    # Additional app features in sidebar
+    interactive_feature_graph(data)  # Call the interactive graph function
 
-    # Visualization with pie chart or bar chart
-    chart_type = st.radio("Select Chart Type", ('Pie Chart', 'Bar Chart'))
-    if chart_type == 'Pie Chart':
-        plt.figure(figsize=(8, 8))
-        plt.pie(feature_data['Count'], labels=feature_data['Category'], autopct='%1.1f%%')
-        plt.title(f'Distribution of {selected_feature_group}')
-        st.pyplot(plt)
-    elif chart_type == 'Bar Chart':
-        plt.figure(figsize=(10, 6))
-        sns.barplot(x='Category', y='Count', data=feature_data)
-        plt.title(f'Distribution of {selected_feature_group}')
-        plt.xticks(rotation=45)
-        st.pyplot(plt)
-
-    # Additional app features
     st.sidebar.header('Dataset Quick Look:')
     st.sidebar.write(f'Number of Datapoints: {data.shape[0]}')
     st.sidebar.write(f'Number of Features: {data.shape[1]}')
+
+
+# Define feature groups globally
+feature_groups = {
+    'PaperlessBilling': ['PaperlessBilling_No', 'PaperlessBilling_Yes'],
+    'Partner': ['Partner_No', 'Partner_Yes'],
+    'InternetService': ['InternetService_DSL', 'InternetService_Fiber optic', 'InternetService_No'],
+    'OnlineBackup': ['OnlineBackup_No', 'OnlineBackup_No internet service', 'OnlineBackup_Yes'],
+    'Dependents': ['Dependents_No', 'Dependents_Yes'],
+    'SeniorCitizen': ['SeniorCitizen_No', 'SeniorCitizen_Yes'],
+    'DeviceProtection': ['DeviceProtection_No', 'DeviceProtection_No internet service', 'DeviceProtection_Yes'],
+    'OnlineSecurity': ['OnlineSecurity_No', 'OnlineSecurity_No internet service', 'OnlineSecurity_Yes'],
+    'TechSupport': ['TechSupport_No', 'TechSupport_No internet service', 'TechSupport_Yes'],
+    'Contract': ['Contract_Month-to-month', 'Contract_One year', 'Contract_Two year'],
+    'StreamingTV': ['StreamingTV_No', 'StreamingTV_No internet service', 'StreamingTV_Yes'],
+    'PaymentMethod': ['PaymentMethod_Bank transfer (automatic)', 'PaymentMethod_Credit card (automatic)',
+                      'PaymentMethod_Electronic check', 'PaymentMethod_Mailed check']
+}
